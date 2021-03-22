1e06ba380963acfa91cfecf13625f518a4c00ac6
LeXik0N <lexik0n1993@yandex.ru>
2021-03-22 21:44:13 +0600

    Solved tasks 1-4 from 3 module

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,38 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (string.trim().length === 0 || size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  const watcher = {
+    count: 0,
+    symbol: ''
+  };
+  let trimmedString = '';
+
+  for (let i = 0; i < string.length; i++) {
+
+    switch (string[i]) {
+      case watcher.symbol:
+        if (watcher.count < size - 1) {
+          trimmedString += string[i];
+          watcher.count++;
+        }
+        break;
+
+      default:
+        trimmedString += string[i];
+        watcher.count = 0;
+        watcher.symbol = string[i];
+        break;
+    }
+
+  }
+
+  return trimmedString;
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,12 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  const uniqueArr = [];
+  const uniqueValues = new Set(arr).values();
 
+  for (const value of uniqueValues) {
+    uniqueArr.push(value);
+  }
+
+  return uniqueArr;
 }
