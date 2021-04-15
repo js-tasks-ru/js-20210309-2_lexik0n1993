3eddbbbe0023be64d0b0ab2ed3856e8958221eee
LeXik0N <lexik0n1993@yandex.ru>
2021-04-16 00:22:30 +0600

    Added solution to task from Module 10

--- 10-routes-browser-history-api/1-dashboard-page/components/column-chart/src/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/components/column-chart/src/index.js
@@ -3,103 +3,79 @@ import fetchJson from '../../../utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart {
-  element;
+  element = null;
   subElements = {};
   chartHeight = 50;
 
   constructor({
-    label = '',
-    link = '',
-    formatHeading = data => data,
     url = '',
     range = {
       from: new Date(),
-      to: new Date(),
-    }
+      to: new Date()
+    },
+    data = [],
+    className = '',
+    label = '',
+    link = '',
+    value = 0,
+    formatHeading = data => `${data}`
   } = {}) {
-    this.url = new URL(url, BACKEND_URL);
+    this.url = url;
     this.range = range;
+    this.data = data;
+    this.className = className;
     this.label = label;
     this.link = link;
+    this.value = value;
     this.formatHeading = formatHeading;
 
     this.render();
+    this.update(this.range.from, this.range.to);
+  }
+
+  get template() {
+    return `
+      <div class="column-chart column-chart_loading column-chart_${this.className}" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          ${this.label}
+          ${this.getLink()}
+        </div>
+
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody(this.data)}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ``;
+  }
+
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      })
+      .join('');
   }
 
   render() {
-    const { from, to } = this.range;
     const element = document.createElement('div');
 
     element.innerHTML = this.template;
 
     this.element = element.firstElementChild;
+
     this.subElements = this.getSubElements(this.element);
-
-    this.loadData(from, to);
-  }
-
-  getHeaderValue(data) {
-    return this.formatHeading(Object.values(data).reduce((accum, item) => (accum + item), 0));
-  }
-
-  async loadData(from, to) {
-    this.element.classList.add('column-chart_loading');
-    this.subElements.header.textContent = '';
-    this.subElements.body.innerHTML = '';
-
-    this.url.searchParams.set('from', from.toISOString());
-    this.url.searchParams.set('to', to.toISOString());
-
-    const data = await fetchJson(this.url);
-
-    this.setNewRange(from, to);
-
-    if (data && Object.values(data).length) {
-      this.subElements.header.textContent = this.getHeaderValue(data);
-      this.subElements.body.innerHTML = this.getColumnBody(data);
-
-      this.element.classList.remove('column-chart_loading');
-    }
-  }
-
-  setNewRange(from, to) {
-    this.range.from = from;
-    this.range.to = to;
-  }
-
-  getColumnBody(data) {
-    const maxValue = Math.max(...Object.values(data));
-
-    return Object.entries(data).map(([key, value]) => {
-      const scale = this.chartHeight / maxValue;
-      const percent = (value / maxValue * 100).toFixed(0);
-      const tooltip = `<span>
-        <small>${key.toLocaleString('default', {dateStyle: 'medium'})}</small>
-        <br>
-        <strong>${percent}%</strong>
-      </span>`;
-
-      return `<div style="--value: ${Math.floor(value * scale)}" data-tooltip="${tooltip}"></div>`;
-    }).join('');
-  }
-
-  getLink() {
-    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
-  }
-
-  get template() {
-    return `
-      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
-        <div class="column-chart__title">
-          Total ${this.label}
-          ${this.getLink()}
-        </div>
-        <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header"></div>
-          <div data-element="body" class="column-chart__chart"></div>
-        </div>
-      </div>
-    `;
   }
 
   getSubElements(element) {
@@ -112,11 +88,50 @@ export default class ColumnChart {
     }, {});
   }
 
-  async update(from, to) {
-    return await this.loadData(from, to);
+  async update(startDate, endDate) {
+    this.startLoading();
+
+    const url = new URL(this.url, BACKEND_URL);
+
+    url.searchParams.set('from', startDate.toJSON());
+    url.searchParams.set('to', endDate.toJSON());
+
+    const response = await fetchJson(url.href);
+    
+    this.data = Object.values(response);
+    
+    this.subElements.body.innerHTML = this.getColumnBody(this.data);
+    this.subElements.header.innerHTML = this.setHeader();
+
+    this.finishLoading();
+  }
+
+  startLoading() {
+    this.element.classList.add('column-chart_loading');
+  }
+
+  finishLoading() {
+    if (this.data.length) {
+      this.element.classList.remove('column-chart_loading');
+    }
+  }
+
+  setHeader() {
+    this.value = this.data.reduce((accum, item) => accum + item);
+
+    return `${this.formatHeading(this.value)}`;
+  }
+
+  remove() {
+    this.element.remove();
   }
 
   destroy() {
-    this.element.remove();
+    if (this.element) {
+      this.remove();
+      this.element = null;
+    }
+
+    this.subElements = {};
   }
 }
--- 10-routes-browser-history-api/1-dashboard-page/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.js
@@ -3,10 +3,173 @@ import SortableTable from './components/sortable-table/src/index.js';
 import ColumnChart from './components/column-chart/src/index.js';
 import header from './bestsellers-header.js';
 
-import fetchJson from './utils/fetch-json.js';
-
-const BACKEND_URL = 'https://course-js.javascript.ru/';
-
 export default class Page {
+  components = {};
+  subElements = {};
 
+  get getTemplate() {
+    return `
+      <div class="dashboard full-height flex-column">
+        <div class="content__top-panel">
+          <h2 class="page-title">Панель управления</h2>
+          <div data-element="rangePicker"></div>
+        </div>
+
+        <div data-element="chartsContainer" class="dashboard__charts">
+          <div data-element="ordersChart" class="dashboard__chart_orders"></div>
+          <div data-element="salesChart" class="dashboard__chart_sales"></div>
+          <div data-element="customersChart" class="dashboard__chart_customers"></div>
+        </div>
+
+        <h3 class="block-title">Лидеры продаж</h3>
+        <div data-element="sortableTable" class="sortable-table"></div>
+      </div>
+    `;
+  }
+
+  get getSubElements() {
+    const subElements = this.element.querySelectorAll('[data-element]');
+
+    return [...subElements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+      return accum;
+    }, {});
+  }
+
+  async render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate;
+
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements;
+    this.subElements.progressBar = document.querySelector('.progress-bar');
+
+    this.initComponents();
+    this.renderComponents();
+    this.addEventListeners();
+
+    // this.finishLoadingData();
+
+    return this.element;
+  }
+
+  async updateComponents(from, to) {
+    const {
+      sortableTable,
+      ordersChart,
+      salesChart,
+      customersChart
+    } = this.components;
+
+    const data = await sortableTable.loadData(
+      sortableTable.sorted.id,
+      sortableTable.sorted.order,
+      0,
+      19,
+      from,
+      to
+    );
+
+    sortableTable.addRows(data);
+    ordersChart.update(from, to);
+    salesChart.update(from, to);
+    customersChart.update(from, to);
+    
+    this.finishLoadingData();
+  }
+
+  finishLoadingData() {
+    this.subElements.progressBar.style.display = 'none';
+  }
+
+  initComponents() {
+    const now = new Date();
+    const to = new Date();
+    const from = new Date(now.setMonth(now.getMonth() - 1));
+
+    const rangePicker = new RangePicker({
+      from,
+      to
+    });
+
+    const ordersChart = new ColumnChart({
+      url: 'api/dashboard/orders',
+      className: 'orders',
+      label: 'Заказы',
+      range: {
+        from,
+        to
+      }
+    });
+
+    const salesChart = new ColumnChart({
+      url: 'api/dashboard/sales',
+      className: 'sales',
+      label: 'Продажи',
+      formatHeading: data => `$${data}`,
+      range: {
+        from,
+        to
+      }
+    });
+
+    const customersChart = new ColumnChart({
+      url: 'api/dashboard/customers',
+      className: 'customers',
+      label: 'Клиенты',
+      range: {
+        from,
+        to
+      }
+    });
+
+    const sortableTable = new SortableTable(header, {
+      url: `api/dashboard/bestsellers`,
+      from,
+      to,
+      start: 0,
+      step: 30,
+      isSortLocally: true
+    });
+
+    this.components = {
+      rangePicker,
+      ordersChart,
+      salesChart,
+      customersChart,
+      sortableTable
+    };
+  }
+
+  renderComponents() {
+    Object.entries(this.components).forEach(([title, component]) => {
+      const container = this.subElements[title];
+
+      container.append(component.element);
+    });
+  }
+
+  addEventListeners() {
+    const { element } = this.components.rangePicker;
+
+    element.addEventListener('date-select', event => {
+      const { from, to } = event.detail;
+
+      // this.subElements.progressBar.style.display = 'block';
+
+      this.updateComponents(from, to);
+    });
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+
+    for (const component of Object.values(this.components)) {
+      component.destroy();
+    }
+  }
 }
